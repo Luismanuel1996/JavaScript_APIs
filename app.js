6dce314fa6310eda51e3692a4bb5a55e0a6a4fa8
Luismanuel1996 <luismanuel1996@live.com>
2022-11-13 00:12:25 -0500

    done

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
-console.log("Hello World!\n==========\n");
+const searchButton = document.getElementById("submitSearch");
+const searchInput = document.getElementById("searchWord");
+const image = document.getElementById("image123");
+const feedBack = document.getElementById("feedback");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+searchButton.addEventListener("click", () => {
+    const searchTerm = searchInput.value;
+
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=grT6gXrLGzNlM3WbLkH6zsvhYiK3T9Pj&s=${searchTerm}`)
+    .then((resolve) => {
+        return resolve.json()
+    })
+    .then((res) => {
+        feedBack.textContent = "";
+        image.src = res.data.images.original.url;
+        searchInput.value = "";
+       
+    })
+    .catch((error) => {
+        //console.error(error);
+        feedBack.textContent = error.message;
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
